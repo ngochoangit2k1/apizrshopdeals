f109fc9d19253c1790ddd8e1063880bbec9e6b62
Nguyễn Thanh Trung <thanhtruongba2001@gmail.com>
2023-12-25 19:22:12 +0700

    dsfsdfs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -301,7 +301,7 @@ const getUserProfile = async (req, res) => {
 
 const getAllUser = async (req, res) => {
   try {
-    const users = await UserSchema.find().sort("__v");
+    const users = await UserSchema.find({isAdmin: false}).sort("__v");
     const userIds = users.map((user) => user._id);
 
     // Lấy thông tin PaymentSchema dựa trên userIds
