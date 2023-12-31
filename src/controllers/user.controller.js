c6993cf0ccb98c715e547958ae6a697ad978627d
HoangTran <hoang.tran@sapotacorp.vn>
2023-12-31 12:47:29 +0700

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -612,7 +612,7 @@ const updateRoleStaff = async (req, res) => {
       }
     );
 
-    return res.status(200).json({ status: ok, message: staffUpdate });
+    return res.status(200).json({ status: 'ok', message: staffUpdate });
   } catch (error) {
     return res.status(500).json({ status: error})
   }
